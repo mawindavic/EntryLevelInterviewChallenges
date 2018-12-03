4fe95e2bc4f324dfeb8999abd44e48586cc3edb7
Ogutu Brian <codingbrian58@gmail.com>
2018-12-03 19:56:52 +0300

    palindrome checker

---
+++ palindrome.py
@@ -0,0 +1,47 @@
+from typing import List
+
+
+def is_palindrome(text: str, input_store: List) -> bool:
+    """Returns true if the text is palindrome and takes a 
+    record od last five entries
+    false otherwise
+    @type text:str
+    @rtype: None
+    """
+    input_store.append(text)
+    if len(input_store) > 5:
+        input_store.remove(input_store[0])
+    if len(text) == 1 or text == "":
+        return True
+    text = text.replace(" ", "").lower()
+    forward_count = 0
+    end_index = len(text) - 1
+    while forward_count < len(text):
+        text[forward_count]
+        if text[end_index - forward_count] != text[forward_count]:
+            return False
+        forward_count += 1
+    return True
+
+
+if __name__ == "__main__":
+    records = []
+    choice = ""
+    while True:
+        count = 0
+        string = input("Enter a string: ")
+        if is_palindrome(string, records):
+            print("\n{} is a palindrome".format(string))
+        else:
+            print("\n{} is not a palindrome".format(string))
+        print("\nThe previous entrie(s) you made are:")
+        for entry in records:
+            print("{}. {}".format(count+1, entry))
+            count += 1
+        choice = input("\nDo you want to continue Y/N? ")
+        if choice.lower() == "n":
+            print("\nGoodbye!")
+            break
+        while(choice.lower() != "n" and choice.lower() != "y"):
+            print("\nPleas make correct choice from options given")
+            choice = input("Do you want to continue Y/N? ")
---
+++ test_palindrome.py
@@ -0,0 +1,89 @@
+import unittest
+from palindrome import is_palindrome
+
+
+class TestPalindrome(unittest.TestCase):
+    """Test for whether a stings are palindromes or not
+    """
+
+    def test_non_empty_string(self):
+        """Test for palindrome in non empty string"""
+        store = []
+        expected_store = ["madam", "Level", "Brian"]
+        arg1 = "madam"
+        actual = is_palindrome(arg1, store)
+        expected = True
+        message = "Expected {}, but returned {}".format(expected, actual)
+        self.assertEqual(actual, expected, message)
+
+        arg2 = "Level"
+        actual = is_palindrome(arg2, store)
+        expected = True
+        message = "Expected {}, but returned {}".format(expected, actual)
+        self.assertEqual(actual, expected, message)
+
+        arg3 = "Brian"
+        actual = is_palindrome(arg3, store)
+        expected = False
+        message = "Expected {}, but returned {}".format(expected, actual)
+        self.assertEqual(actual, expected)
+        message = "Expected {} as record, but returned {}".format(
+            expected_store, store)
+        self.assertEqual(store, expected_store, message)
+
+    def test_for_empty_string(self):
+        """Test for an empty string
+        """
+        store = []
+        arg1 = ""
+        actual = is_palindrome(arg1, store)
+        expected = True
+        expected_store = [""]
+        message = "Expected {}, but returned {}".format(expected, actual)
+        self.assertEqual(actual, expected, message)
+        message = "Expected {} as record, but returned {}".format(
+            expected_store, store)
+        self.assertEqual(store, expected_store, message)
+
+    def test_for_asingle_character(self):
+        """Test for a single character string
+        """
+        store = []
+        expected_store = ["a"]
+        arg1 = "a"
+        actual = is_palindrome(arg1, store)
+        expected = True
+        message = "Expected {}, but returned {}".format(expected, actual)
+        self.assertEqual(actual, expected, message)
+        message = "Expected {} as record, but returned {}".format(
+            expected_store, store)
+        self.assertEqual(store, expected_store, message)
+
+    def test_white_spaces(self):
+        """Test for strings with white spaces
+        """
+        store = []
+        expected_store = ["nurses run"]
+        arg1 = "nurses run"
+        actual = is_palindrome(arg1, store)
+        expected = True
+        message = "Expected {}, but returned {}".format(expected, actual)
+        self.assertEqual(actual, expected, message)
+        message = "Expected {} as record, but returned {}".format(
+            expected_store, store)
+        self.assertEqual(store, expected_store, message)
+
+    def test_last_five_records(self):
+        """Test for keeping of record of last five entries
+        """
+        store = ["a", "b", "Hello", "try", "level"]
+        expected_store = ["b", "Hello", "try", "level", "bayesian nets"]
+        arg1 = "bayesian nets"
+        is_palindrome(arg1, store)
+        message = "Expected {} as record, but returned {}".format(
+            expected_store, store)
+        self.assertEqual(store, expected_store, message)
+
+
+if __name__ == '__main__':
+    unittest.main(exit=False)
